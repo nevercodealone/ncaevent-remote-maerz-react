bce8765d689d84aee098670fbec6d415fac48570
Stefan Adolf <stefan.adolf@turbinekreuzberg.com>
2020-03-27 17:15:43 +0100

    using listgroup items

--- spa/src/App.js
+++ spa/src/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import { Col, Row } from 'react-bootstrap';
-import Layout from './Layout';
+import React from "react";
+import { Col, Row, ListGroup, Button } from "react-bootstrap";
+import Layout from "./Layout";
 
+const events = [{ title: "test1" }, { title: "test2" }, { title: "test3" }];
 
 const App = () => (
   <Layout>
     <h2>Events!</h2>
+    <ListGroup>
+      {events.map(event => (
+        <ListGroup.Item action href="">
+          {event.title}
+        </ListGroup.Item>
+      ))}
+    </ListGroup>
+    <Button>Create a new Event</Button>
   </Layout>
-)
+);
 
 export default App;
