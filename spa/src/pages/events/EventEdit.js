d3823547b7bb9004ef5ad27cb6910696ca9a64fe
Stefan Adolf <stefan.adolf@turbinekreuzberg.com>
2020-03-27 20:57:46 +0100

    url validation

--- spa/src/pages/events/EventEdit.js
+++ spa/src/pages/events/EventEdit.js
@@ -2,10 +2,21 @@ import React, { useState } from "react";
 import events from "../../events.json";
 import { Form } from "react-bootstrap";
 
+const URL_REGEX = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/;
+
 const EventForm = ({ event }) => {
   const [title, setTitle] = useState(event.title);
   const [url, setUrl] = useState(event.url || "");
+  const [isUrlValid, setIsUrlValid] = useState(validateUrl(event.url || ""));
 
+  function validateUrl(possiblyAnUrl) {
+    return URL_REGEX.test(possiblyAnUrl);
+  }
+
+  function setValidatedUrl(url) {
+    setUrl(url);
+    setIsUrlValid(validateUrl(url));
+  }
   return (
     <Form>
       <h2>{title}</h2>
@@ -17,13 +28,20 @@ const EventForm = ({ event }) => {
           onChange={evt => setTitle(evt.target.value)}
         ></Form.Control>
       </Form.Group>
-      <Form.Group>
-        <Form.Label>Url</Form.Label>
+      <Form.Group controlId="url">
+        <Form.Label>
+          Url {url} {isUrlValid ? 1 : 0}
+        </Form.Label>
         <Form.Control
           name="url"
           value={url}
-          onChange={evt => setUrl(evt.target.value)}
-        ></Form.Control>
+          isInvalid={!isUrlValid}
+          isValid={isUrlValid}
+          onChange={evt => setValidatedUrl(evt.target.value)}
+        />
+        <Form.Control.Feedback type="invalid">
+          That URL seems invalid
+        </Form.Control.Feedback>
       </Form.Group>
     </Form>
   );
