327a16f602f234820d5170101aeba8be4945d909
Stefan Adolf <stefan.adolf@turbinekreuzberg.com>
2020-03-27 21:39:16 +0100

    using global event state

--- spa/src/App.js
+++ spa/src/App.js
@@ -1,17 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import Layout from "./Layout";
 import EventIndex from "./pages/events/EventIndex";
 import { Router, Redirect } from "@reach/router";
 import EventEdit from "./pages/events/EventEdit";
+import initialEvents from "./events.json";
 
-const App = () => (
-  <Layout>
-    <Router>
-      <Redirect from="/" to="/events" noThrow />
-      <EventIndex path="/events" />
-      <EventEdit path="/events/:eventId" />
-    </Router>
-  </Layout>
-);
+const App = () => {
+  const [events, setEvents] = useState(initialEvents);
+  return (
+    <Layout>
+      <Router>
+        <Redirect from="/" to="/events" noThrow />
+        <EventIndex path="/events" events={events} />
+        <EventEdit
+          path="/events/:eventId"
+          events={events}
+          onSubmit={events => setEvents(events)}
+        />
+      </Router>
+    </Layout>
+  );
+};
 
 export default App;
