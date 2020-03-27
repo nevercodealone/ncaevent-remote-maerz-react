c063f1160e5b9e9419d890a157db358b81ac6b43
Stefan Adolf <stefan.adolf@turbinekreuzberg.com>
2020-03-27 17:00:36 +0100

    using bootstrap / scss builds basic layout

--- spa/src/App.js
+++ spa/src/App.js
@@ -1,26 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Col, Row } from 'react-bootstrap';
+import Layout from './Layout';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+const App = () => (
+  <Layout>
+    <h2>Events!</h2>
+  </Layout>
+)
 
 export default App;
